e0af4c5c6e189bcc8f6ed5e0895289aaa2e31cce
Qwatrum <171450552+Qwatrum@users.noreply.github.com>
2025-06-25 15:22:05 +0200

    this should work

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -14,6 +14,7 @@ const eslintConfig = [
   {
     rules: {
       "react/no-unescaped-entities": "off",
+      "@typescript-eslint/no-explicit-any": "off",
       "@typescript-eslint/no-unused-vars": "off",
       "@next/next/no-html-link-for-pages": "off",
     },
